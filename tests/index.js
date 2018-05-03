db52dd72ba46cd334a4bd09f11ae8e2aefd69d4a
adam_giacomelli <adam.giacom@gmail.com>
2018-05-03 18:55:29 +0200

    Added schema description support

--- tests/index.js
+++ tests/index.js
@@ -1,35 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const port = process.env.PORT || 1234;
 const app = express();
-const swaggerGenerator = require('../index.js');
+const swaggerGenerator = require("../index.js");
 
 const expressSwagger = swaggerGenerator(app);
 const options = {
   swaggerDefinition: {
     info: {
-      description: 'This is the generator test',
-      title: 'Swagger generator test',
-      version: '1.0.0'
+      description: "This is the generator test",
+      title: "Swagger generator test",
+      version: "1.0.0"
     },
-    host: 'localhost:' + port,
-    basePath: '/api/v1',
-    produces: ['application/json', 'application/xml'],
+    host: "localhost:" + port,
+    basePath: "/api/v1",
+    produces: ["application/json", "application/xml"],
     schemes:
-      process.env.NODE_ENV === 'development' ? ['http', 'https'] : ['https']
+      process.env.NODE_ENV === "development" ? ["http", "https"] : ["https"]
   },
-  docUrl: '/docs',
+  docUrl: "/docs",
   basedir: __dirname, // app absolute path
-  files: ['./**/*.js'] // Path to the API handle folder
+  files: ["./**/*.js"] // Path to the API handle folder
 };
 
 try {
-  console.log('Setting up Swagger generator');
+  console.log("Setting up Swagger generator");
   expressSwagger(options);
-  console.log('Done');
 } catch (err) {
-  console.warn(`Swagger failed: ${err}`);
+  console.error(`Swagger failed: ${err}`);
 }
 
 /**
@@ -44,10 +43,11 @@ try {
  * @title Get a testy test
  * @group Generator - Testing
  *
+ * @param {String} AnotherTestyTest - The interesting string parameter
  * @param {AVeryTestyType.model} averytestytype.body - The type
  *
  */
-app.get('/', (req, res) => res.status(200).send({ message: 'Sanity test' }));
+app.get("/", (req, res) => res.status(200).send({ message: "Sanity test" }));
 
 app.listen(port, () => {
   console.log(`Express server listening on port: ${port}`);
